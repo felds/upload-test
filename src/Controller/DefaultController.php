8906b447b62cf542d6cf8c6612d5b82b8b7be865
Felds Liscia <dev@felds.com.br>
2018-03-20 19:07:45 -0300

    add homepage

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('Default/index.html.twig');
+    }
+}
