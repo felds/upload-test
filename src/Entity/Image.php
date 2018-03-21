7b6249cbdb28814a8a1818fe8e4aaa46a5d915f8
Felds Liscia <dev@felds.com.br>
2018-03-21 15:06:56 -0300

    create image data mapper

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @ORM\Entity()
@@ -20,7 +21,32 @@ class Image
 
     /**
      * @var string
-     * @ORM\Column(nullable=true)
+     * @ORM\Column()
      */
     private $path;
+
+    /**
+     * Image constructor.
+     * @param File $file
+     */
+    public function __construct(File $file)
+    {
+        $this->path = $file->getRealPath();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
 }
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -5,8 +5,12 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataMapperInterface;
+use Symfony\Component\Form\Exception;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImageType extends AbstractType
@@ -15,13 +19,35 @@ class ImageType extends AbstractType
     {
         $builder->add('file', FileType::class, [
             'mapped' => false,
+            'required' => false,
         ]);
+
+        $builder->setDataMapper(new ImageDataMapper());
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Image::class,
+            'empty_data' => null,
         ]);
+    }
+}
+
+class ImageDataMapper implements DataMapperInterface
+{
+    public function mapDataToForms($data, $forms)
+    {
+        // noop
+    }
+
+    public function mapFormsToData($forms, &$data)
+    {
+        $forms = iterator_to_array($forms);
+        $file = $forms['file']->getData();
+
+        if ($file instanceof File) {
+            $data = new Image($file);
+        }
     }
 }
